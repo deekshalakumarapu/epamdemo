d0dfc548bbc384747e504f31adf53c30b98357b0
Pandu <diksha.lakumarapu@gmail.com>
2020-02-20 20:30:21 +0530

    first commit

---
+++ TASK3/src/main/java/task3/Choclate.java
@@ -0,0 +1,8 @@
+package task3;
+
+public class Choclate extends NewYearGift
+{
+    public Choclate(String name, int quantity) {
+        super(name, quantity);
+    }
+}
---
+++ TASK3/src/main/java/task3/Sweet.java
@@ -0,0 +1,9 @@
+package task3;
+
+public class Sweet extends NewYearGift
+{
+    public Sweet(String name, int quantity) {
+        super(name, quantity);
+    }
+
+}
